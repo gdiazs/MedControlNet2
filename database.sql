f39e3f35a72b95aefc5cafe58fcf4de59daf72bc
gdiazs <diazgbs@gmail.com>
2022-11-03 21:37:34 -0600

    Actualiza Base de datos

--- database.sql
+++ database.sql
@@ -10,14 +10,14 @@ GO
 USE MedControlNetDB
 GO
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'EspecialidadMedica')
-	CREATE TABLE dbo.EspecialidadMedica
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Especialidad')
+	CREATE TABLE dbo.Especialidad
 	(
-		EspecialidadMedicaID INT IDENTITY(1,1),
+		EspecialidadID INT IDENTITY(1,1),
 		NombreEspecialidad VARCHAR(100),
 		CONSTRAINT PK_EspecialidadMedica PRIMARY KEY  CLUSTERED 
 		(
-			EspecialidadMedicaID
+			EspecialidadID
 		)
 	)
 	GO
@@ -27,7 +27,7 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Consultorio')
 	CREATE TABLE dbo.Consultorio
 	(
 		ConsultorioID INT IDENTITY(1,1),
-		NombreDeConsultorio VARCHAR(80),
+		NumeroConsultorio INT,
 		CONSTRAINT PK_Consultorio PRIMARY KEY  CLUSTERED 
 		(
 			ConsultorioID
@@ -40,11 +40,11 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'ConsultorioEspecialidad' a
 	CREATE TABLE dbo.ConsultorioEspecialidad
 	(
 		ConsultorioID INT, 
-		EspecialidadMedicaID INT,
+		EspecialidadID INT,
 		CONSTRAINT PK_MatriculaDeCurso PRIMARY KEY  CLUSTERED 
 		(
 			ConsultorioID,
-			EspecialidadMedicaID
+			EspecialidadID
 		),
 		CONSTRAINT FK_ConsultorioEspecialidad_ConsultorioID FOREIGN KEY 
 		(
@@ -53,12 +53,12 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'ConsultorioEspecialidad' a
 		) REFERENCES dbo.Consultorio (
 			ConsultorioID
 		),
-		CONSTRAINT FK_ConsultorioEspecialidad_EspecialidadMedicaID FOREIGN KEY 
+		CONSTRAINT FK_ConsultorioEspecialidad_EspecialidadID FOREIGN KEY 
 		(
-			EspecialidadMedicaID
+			EspecialidadID
 
-		) REFERENCES dbo.EspecialidadMedica (
-			EspecialidadMedicaID
+		) REFERENCES dbo.Especialidad (
+			EspecialidadID
 		)
 	)
 	GO
@@ -66,46 +66,95 @@ GO
 
 
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'MedicoEspecialista')
-	CREATE TABLE dbo.MedicoEspecialista
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Medico')
+	CREATE TABLE dbo.Medico
 	(
-		MedicoEspecialistaID BIGINT IDENTITY(1,1),
+		MedicoID INT IDENTITY(1,1),
+		Identificacion VARCHAR(30),
 		Nombre VARCHAR(255),
 		EsTemporal BIT,
-		EspecialidadMedicaID INT, 
+		EspecialidadID INT, 
 		
-		CONSTRAINT PK_MedicoEspecialista PRIMARY KEY  CLUSTERED 
+		CONSTRAINT PK_Medico PRIMARY KEY  CLUSTERED 
 		(
-			MedicoEspecialistaID
+			MedicoID
 		),
-		CONSTRAINT FK_MedicoEspecialista_EspecialidadMedicaID FOREIGN KEY 
+		CONSTRAINT FK_Medico_EspecialidadID FOREIGN KEY 
 		(
-			EspecialidadMedicaID
+			EspecialidadID
 
-		) REFERENCES dbo.EspecialidadMedica (
-			EspecialidadMedicaID
+		) REFERENCES dbo.Especialidad (
+			EspecialidadID
 		)
 	)
 	GO
 GO
 
-INSERT INTO Consultorio
-		(NombreDeConsultorio)
-		VALUES 
-			('Arenal'),
-			('Barva'),
-			('Miravalles'),
-			('Tenorio'),
-			('Poas'),
-			('Irazu'),
-			('Turrialba'),
-			('Chato'),
-			('Orosi'),
-			('Platanar')
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Paciente')
+	CREATE TABLE dbo.Paciente
+	(
+		PacienteID INT IDENTITY(1,1),
+		Identificacion VARCHAR(30),
+		Nombre VARCHAR(255),
+		CONSTRAINT PK_Paciente PRIMARY KEY  CLUSTERED 
+		(
+			PacienteID
+		)
+	)
+	GO
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Cita')
+	CREATE TABLE dbo.Cita
+	(
+		CitaID INT IDENTITY(1,1),
+		PacienteID INT, 
+		EspecialidadID INT,
+		HoraCita DATETIME,
+		Costo DECIMAL(19,4),
+		CONSTRAINT PK_Cita PRIMARY KEY  CLUSTERED 
+		(
+			CitaID,
+			PacienteID,
+			EspecialidadID
+		),
+		CONSTRAINT FK_Cita_PacienteID FOREIGN KEY 
+		(
+			PacienteID
+
+		) REFERENCES dbo.Paciente (
+			PacienteID
+		),
+		CONSTRAINT FK_Cita_EspecialidadID FOREIGN KEY 
+		(
+			EspecialidadID
+
+		) REFERENCES dbo.Especialidad (
+			EspecialidadID
+		)
+	)
+	GO
 GO
 
 
-INSERT INTO EspecialidadMedica
+INSERT INTO Consultorio
+		(NumeroConsultorio)
+		VALUES 
+			(1),
+			(2),
+			(3),
+			(4),
+			(5),
+			(6),
+			(7),
+			(8),
+			(9),
+			(10)
+GO
+
+
+INSERT INTO Especialidad
            (NombreEspecialidad)
      VALUES
 		('Medicina General'),
