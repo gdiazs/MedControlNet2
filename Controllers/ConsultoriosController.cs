a3715402e5e0bfad52f4937d9176cc030836c92a
gdiazs <diazgbs@gmail.com>
2022-11-15 21:48:36 -0600

    Agrega editar consultorio, falta especialidades

--- Controllers/ConsultoriosController.cs
+++ Controllers/ConsultoriosController.cs
@@ -1,17 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MedControlNet.Entities;
 
 namespace MedControlNet.Controllers
 {
     public class ConsultoriosController : Controller
     {
-        // GET: Consultorios
+        private MedControlNetDBEntities db = new MedControlNetDBEntities();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Consultorios.ToList());
+        }
+
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consultorio consultorio = db.Consultorios.Find(id);
+            if (consultorio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consultorio);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "ConsultorioID,NumeroConsultorio")] Consultorio consultorio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consultorio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(consultorio);
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Models/ConsultorioModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedControlNet.Models
+{
+    public class ConsultorioModelo
+    {
+        public string Consultorio { set; get; }
+        public string Especialidad { set; get; }
+
+    }
+}
