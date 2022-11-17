544a25038ebec08fad6e4fa261d7258f0f96f280
gdiazs <diazgbs@gmail.com>
2022-11-16 20:30:58 -0600

    Refactoriza a Servicio

--- Controllers/ConsultoriosController.cs
+++ Controllers/ConsultoriosController.cs
@@ -9,16 +9,23 @@ using System.Web;
 using System.Web.Mvc;
 using MedControlNet.Entities;
 using MedControlNet.Models;
+using MedControlNet.Services;
 
 namespace MedControlNet.Controllers
 {
     public class ConsultoriosController : Controller
     {
-        private MedControlNetDBEntities db = new MedControlNetDBEntities();
+
+        private readonly ConsultoriosServicio _consultoriosServicio;
+
+        public ConsultoriosController(ConsultoriosServicio consultoriosServicio)
+        {
+            _consultoriosServicio = consultoriosServicio;
+        }
 
         public ActionResult Index()
         {
-            return View(db.Consultorios.ToList());
+            return View(_consultoriosServicio.ObtenerConsultorios());
         }
 
 
@@ -29,77 +36,37 @@ namespace MedControlNet.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var consultorio = db.Consultorios.Find(id);
 
+            return View(_consultoriosServicio.ObtenerConsutultorioPorId(id));
 
-            var listaEspecialidades = consultorio.Especialidads.Select(especialidad => especialidad.EspecialidadID).ToList();
-
-            var especialidades = db.Especialidads
-                .Where( especialidad => !listaEspecialidades.Contains(especialidad.EspecialidadID)).ToList();
-
-            var items = especialidades
-                .Select(especialidad => new SelectListItem() { Value = especialidad.EspecialidadID + "", Text = especialidad.NombreEspecialidad })
-                .ToList();
-
-
-            var consultorioModelo = new ConsultorioModelo()
-            {
-                ConsultorioID = consultorio.ConsultorioID,
-                NumeroConsultorio = consultorio.NumeroConsultorio.Value,
-                EspecialidadesConsultorio = consultorio.Especialidads,
-                TodasLasEspecialidades = items
-
-            };
-            return View(consultorioModelo);
         }
 
         [HttpPost]
         public ActionResult Edit([Bind(Include = "ConsultorioID,NumeroConsultorio")] ConsultorioModelo consultorioModelo)
         {
-            var consultorio = db.Consultorios.Find(consultorioModelo.ConsultorioID);
+           
             if (ModelState.IsValid)
             {
 
-                consultorio.NumeroConsultorio = consultorioModelo.NumeroConsultorio;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                _consultoriosServicio.ActualizarConsultorio(consultorioModelo);
             }
-            return View(consultorio);
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         public ActionResult AgregarEspecialidad(ConsultorioModelo consultorioModelo) {
 
-            var consultorio = db.Consultorios.Find(consultorioModelo.ConsultorioID);
-            var especialidad = db.Especialidads.Find(consultorioModelo.NuevaEspecialidad);
-            consultorio.Especialidads.Add(especialidad);
-
-            db.SaveChanges();
+            _consultoriosServicio.AgregarEspecialidad(consultorioModelo);
 
             return RedirectToAction($"Edit/{consultorioModelo.ConsultorioID}");
         }
 
         public ActionResult EliminarEspecialidad(int? especialidadId, int? consultorioId) {
 
-            var consultorio = db.Consultorios.Find(consultorioId);
-            var especialidad = db.Especialidads.Find(especialidadId);
-
-            consultorio.Especialidads.Remove(especialidad);
-
-            db.SaveChanges();
+            _consultoriosServicio.RemoverEspecialidad(especialidadId, consultorioId);
 
             return RedirectToAction($"Edit/{consultorioId}");
         }
 
-
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
---
+++ Services/ConsultoriosServicio.cs
@@ -0,0 +1,90 @@
+﻿using MedControlNet.Entities;
+using MedControlNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MedControlNet.Services
+{
+    public class ConsultoriosServicio
+    {
+
+        public ConsultorioModelo ObtenerConsutultorioPorId(int? consultorioId) 
+        {
+
+            using (var db = new MedControlNetDBEntities()) 
+            {
+
+                var consultorio = db.Consultorios.Find(consultorioId);
+
+
+                var listaEspecialidades = consultorio.Especialidads.Select(especialidad => especialidad.EspecialidadID).ToList();
+
+                var especialidades = db.Especialidads
+                    .Where(especialidad => !listaEspecialidades.Contains(especialidad.EspecialidadID)).ToList();
+
+                var items = especialidades
+                    .Select(especialidad => new SelectListItem() { Value = especialidad.EspecialidadID + "", Text = especialidad.NombreEspecialidad })
+                    .ToList();
+
+
+                var consultorioModelo = new ConsultorioModelo()
+                {
+                    ConsultorioID = consultorio.ConsultorioID,
+                    NumeroConsultorio = consultorio.NumeroConsultorio.Value,
+                    EspecialidadesConsultorio = consultorio.Especialidads,
+                    TodasLasEspecialidades = items
+
+                };
+                return consultorioModelo;
+            }
+
+        }
+
+        public Consultorio ActualizarConsultorio(ConsultorioModelo consultorioModelo) 
+        {
+            using (var db = new MedControlNetDBEntities())
+            {
+                var consultorio = db.Consultorios.Find(consultorioModelo.ConsultorioID);
+                consultorio.NumeroConsultorio = consultorioModelo.NumeroConsultorio;
+                db.SaveChanges();
+                return consultorio;
+            }
+
+        }
+
+        public void AgregarEspecialidad(ConsultorioModelo consultorioModelo) 
+        {
+            using (var db = new MedControlNetDBEntities()) {
+
+                var consultorio = db.Consultorios.Find(consultorioModelo.ConsultorioID);
+                var especialidad = db.Especialidads.Find(consultorioModelo.NuevaEspecialidad);
+                consultorio.Especialidads.Add(especialidad);
+
+                db.SaveChanges();
+            }
+        }
+
+        public void RemoverEspecialidad(int? especialidadId, int? consultorioId) 
+        {
+            using (var db = new MedControlNetDBEntities())
+            {
+                var consultorio = db.Consultorios.Find(consultorioId);
+                var especialidad = db.Especialidads.Find(especialidadId);
+
+                consultorio.Especialidads.Remove(especialidad);
+
+                db.SaveChanges();
+            }
+
+        }
+
+        public List<Consultorio> ObtenerConsultorios()
+        {
+            using (var db = new MedControlNetDBEntities()) {
+                return db.Consultorios.ToList();
+            }
+        }
+    }
+}
